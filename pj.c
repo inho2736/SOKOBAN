8fdc07b0cc91d1db8cedcf2dfc2c2946688e3609
inho2736 <limcara@nate.com>
2017-06-12 22:07:08 +0900

    Create pj.c

--- pj.c
+++ pj.c
@@ -17,6 +17,7 @@ int getch(void);
 void mapscan(void);
 void mapopen(void);
 void stagescan(void);
+void move(void);
 int getch(void)//엔터없이 방향키와 옵션을 입력받는데 쓰는 getch함수
 {
 	int ch;
@@ -88,7 +89,222 @@ void stagescan(void)
 }
 
 }
+void move(void)
+{
+    x1=cx[p], y1=cy[p]; // 처음 커서위치를 배열에서 불러옴
 
+		char k;// 커서이동과 옵션 입력받는 변수
+    begin_undo();//
+
+		while (1){
+
+			k = getch();
+
+			if (k=='h'){
+
+				if (stage[l1][y1][x1-1]=='#')
+					continue;// 벽이 옆일경무 무시
+
+				else if (stage[l1][y1][x1-1]==' '){// 빈공간이 옆일경우
+
+					if (map[l1][y1][x1]=='O'){ // 창고지기의 자리가 보관장소였을때
+						stage[l1][y1][x1-1]='@';// 창고지기가 왼쪽으로 이동
+						stage[l1][y1][x1]='O';// 원래 창고지기의 자리에 보관장소 다시저장
+						x1--;// 창고지기의 가로좌표축 -1
+						print();}// 새맵 출력
+					else {// 창고지기의 자리가 보관장소가 아니면
+
+					stage[l1][y1][x1-1]='@'; // 왼쪽으로 창고지기 이동
+					stage[l1][y1][x1]=' ';// 원래자리에 빈칸 다시저장
+					x1--;// 창고지기의 가로좌표축 -1
+					print();}//새맵 출력
+				}
+
+				else if (stage[l1][y1][x1-1]=='$'){// 창고지기 왼쪽이 상자(금)이면
+
+					if (stage[l1][y1][x1-2]==' '){// 그 왼쪽이 빈칸이면
+
+						stage[l1][y1][x1-2]='$';
+						stage[l1][y1][x1-1]='@';
+						stage[l1][y1][x1]=' ';
+						x1--;
+					print();
+				} //' ' < '$' < '@' 이런식으로 이동
+
+					else if (stage[l1][y1][x1-2]=='O'){// 그왼쪽이 보관상자이면
+
+						stage[l1][y1][x1-2]='$';//
+						stage[l1][y1][x1-1]='@';
+						stage[l1][y1][x1]=map[l1][y1][x1];
+						x1--;
+						//위와같이이동하되 원래 창고지기의 자리에 맵에 원래 저장된 보관상자 저장
+					print();
+					}
+
+					else
+						continue;// 그 왼쪽이 벽일경우 무시
+				}
+				else {// 그 왼쪽이 보관장소일 경우
+
+					stage[l1][y1][x1-1]='@';// 보관장소자리에 창고지기 두기
+					stage[l1][y1][x1]=map[l1][y1][x1];// 원래 창고지기자리에 원래 맵 출력
+					x1--;
+					print();
+				}}
+
+
+			if (k=='l'){// 오른쪽으로 이동시(위와 동일)
+
+				if (stage[l1][y1][x1+1]=='#')
+					continue;
+
+				else if (stage[l1][y1][x1+1]==' '){
+
+
+					if (map[l1][y1][x1]=='O'){
+						stage[l1][y1][x1+1]='@';
+						stage[l1][y1][x1]='O';
+						x1++;
+						print();}
+					else
+
+					{stage[l1][y1][x1+1]='@';
+					stage[l1][y1][x1]=' ';
+					x1++;
+					print();}
+				}
+
+				else if (stage[l1][y1][x1+1]=='$'){
+
+					if (stage[l1][y1][x1+2]==' '){
+
+						stage[l1][y1][x1+2]='$';
+						stage[l1][y1][x1+1]='@';
+						stage[l1][y1][x1]=' ';
+						x1++;
+					print();
+					} //' ' < '$' < '@'
+
+
+					else if (stage[l1][y1][x1+2]=='O'){
+
+						stage[l1][y1][x1+2]='$';
+						stage[l1][y1][x1+1]='@';
+						stage[l1][y1][x1]=map[l1][y1][x1];
+						x1++;
+						print();
+					}
+
+				}
+				else {				stage[l1][y1][x1+1]='@';
+					stage[l1][y1][x1]=map[l1][y1][x1];
+					x1++;
+
+					print();
+				}
+
+			}
+
+			if (k=='k'){// 위쪽으로 이동시(위와 동일)
+
+				if (stage[l1][y1-1][x1]=='#')
+					continue;
+
+				else if (stage[l1][y1-1][x1]==' '){
+
+					if (map[l1][y1][x1]=='O'){
+						stage[l1][y1-1][x1]='@';
+						stage[l1][y1][x1]='O';
+						y1--;
+						print();}
+					else
+
+					{stage[l1][y1-1][x1]='@';
+					stage[l1][y1][x1]=' ';
+					y1--;
+					print();}}
+
+				else if (stage[l1][y1-1][x1]=='$'){
+
+					if (stage[l1][y1-2][x1]==' '){
+
+						stage[l1][y1-2][x1]='$';
+						stage[l1][y1-1][x1]='@';
+						stage[l1][y1][x1]=' ';
+						y1--;
+
+					print();
+					} //' ' < '$' < '@'
+
+					else if (stage[l1][y1-2][x1]=='O'){
+
+						stage[l1][y1-2][x1]='$';
+						stage[l1][y1-1][x1]='@';
+						stage[l1][y1][x1]=map[l1][y1][x1];
+						y1--;
+					print();
+					}
+
+					else
+						continue;
+				}
+				else if (stage[l1][y1-1][x1]=='O'){
+					stage[l1][y1-1][x1]='@';
+					stage[l1][y1][x1]=map[l1][y1][x1];
+					y1--;
+					print();}
+
+			}
+			if (k=='j'){// 아랫쪽으로 이동시(위와 동일)
+
+				if (stage[l1][y1+1][x1]=='#')
+					continue;
+
+				else if (stage[l1][y1+1][x1]==' '){
+
+					if (map[l1][y1][x1]=='O'){
+						stage[l1][y1+1][x1]='@';
+						stage[l1][y1][x1]='O';
+						y1++;
+						print();}
+					else
+
+					{stage[l1][y1+1][x1]='@';
+					stage[l1][y1][x1]=' ';
+					y1++;
+					print();}}
+
+				else if (stage[l1][y1+1][x1]=='$'){
+
+					if (stage[l1][y1+2][x1]==' '){
+
+						stage[l1][y1+2][x1]='$';
+						stage[l1][y1+1][x1]='@';
+						stage[l1][y1][x1]=' ';
+						y1++;
+					print();
+					} //' ' < '$' < '@'
+
+					else if (stage[l1][y1+2][x1]=='O'){
+
+						stage[l1][y1+2][x1]='$';
+						stage[l1][y1+1][x1]='@';
+						stage[l1][y1][x1]=map[l1][y1][x1];
+						y1++;
+					print();
+
+					}
+
+					else
+						continue;
+				}
+				else if (stage[l1][y1+1][x1]=='O'){
+					stage[l1][y1+1][x1]='@';
+					stage[l1][y1][x1]=map[l1][y1][x1];
+					y1++;
+					print();
+				}
+			}
 
 
 
